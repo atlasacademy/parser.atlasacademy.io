647861cc64300ca95ba784e9536939bc9f46f011
sadisticsolution <sadisticsolutione@gmail.com>
2020-09-14 13:51:58 -0700

    Bump up time for match job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new AppendSubmissionsJob())->everyThirtyMinutes();
+        $schedule->job(new AppendSubmissionsJob())->everyFiveMinutes();
     }
 
     /**
